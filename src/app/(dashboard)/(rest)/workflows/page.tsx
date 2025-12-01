f34c9444debcdd8652e3d4009e36a6472493567f
Nuwan0612 <nuwanfernando17@gmail.com>
2025-12-01 16:14:18 +0530

    Workflow Paginations

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,15 +1,22 @@
 import { WorkFlowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+import { workflowsParamsLoader } from "@/features/workflows/server/params-loader";
 import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils"
 import { HydrateClient } from "@/trpc/server";
+import { se } from "date-fns/locale";
+import type { SearchParams } from "nuqs";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary"
 
+type Props = {
+  searchParams: Promise<SearchParams>
+}
 
-const Page = async () => {
+const Page = async ({ searchParams }: Props) => {
   await requireAuth();
 
-  prefetchWorkflows()
+  const params = await workflowsParamsLoader(searchParams);
+  prefetchWorkflows(params)
 
   return (
     <WorkFlowsContainer>
