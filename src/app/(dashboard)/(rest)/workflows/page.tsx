28dd7b4d4a4d22b9ae538058fc92c6cd6b04832a
Nuwan0612 <nuwanfernando17@gmail.com>
2025-12-01 11:53:00 +0530

    workflows crud

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,11 +1,26 @@
+import { WorkFlowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils"
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary"
 
 
 const Page = async () => {
-  await requireAuth()
+  await requireAuth();
+
+  prefetchWorkflows()
 
   return (
-    <div>Workflows</div>
+    <WorkFlowsContainer>
+      <HydrateClient>
+        <ErrorBoundary fallback={<p>Error!</p>}>
+          <Suspense fallback={<p>Loading workflows...</p>}>
+            <WorkflowsList />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </WorkFlowsContainer>
   )
 }
 
